f02d8172e78cfbfc25924d69fe8ac1c483496680
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-08 18:34:05 -0400

    inital commit

--- RoomDetailGenerator/Program.cs
+++ RoomDetailGenerator/Program.cs
@@ -10,6 +10,40 @@ namespace RoomDetailGenerator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+            string input;
+            do
+            {
+                Console.WriteLine("Please enter the length");
+                string length = Console.ReadLine();
+                double l = double.Parse(length);
+
+                Console.WriteLine("Please enter the width");
+                string width = Console.ReadLine();
+                double w = double.Parse(width);
+
+                Console.WriteLine("Please enter the height");
+                string height = Console.ReadLine();
+                double h = double.Parse(height);
+
+                double p;
+                double a;
+                double v;
+
+                p = 2 * l + 2 * w;
+                Console.WriteLine("Perimeter: " + p);
+
+                a = l * w;
+                Console.WriteLine("Area: " + a);
+
+                v = l * w * h;
+                Console.WriteLine("Volume: " + v);
+
+                Console.WriteLine("Do you want to continue measuring rooms? y/n");
+                input = Console.ReadLine();
+            }
+            while (input == "y");
         }
+  
     }
 }
